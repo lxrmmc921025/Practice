cc80c6889d893681df2316fe176970ac9b3742e0
lxrmmc921025 <zxbjl130326@gmail.com>
2018-06-28 23:49:26 -0400

    only maintain end of interval

--- 763.java
+++ 763.java
@@ -6,41 +6,27 @@ class Solution {
     */
     public List<Integer> partitionLabels(String S) {
         List<Integer> res = new ArrayList<>();
-        int[][] map = new int[26][2];
-        
+        int[] map = new int[26];
         
         char[] ch = S.toCharArray();
+        //map only maintain last pos of each char
         for (int i = 0; i < ch.length; i++) {
-            int[] pos = map[ch[i] - 'a'];
-            if(pos[0] != -1) {
-                pos[1] = i;
-            } else {
-                pos[0] = i;
-                pos[1] = i;
-            }
+            map[ch[i] - 'a'] = i;
         }
         
         int start = 0, end = 0;
         for (int i = 0; i < ch.length; i++) {
-            int ts = map[ch[i] - 'a'][0];
-            int te = map[ch[i] - 'a'][1];
+            int ce = map[ch[i] - 'a'];
             
-            if (te == end) {
-                //same char with char interval [ts, te] will terminate this part e.g "a.._a_..a"
-                if (i < end) {
-                    continue;
-                }
+            if (ce == end) {
                 res.add(end - start + 1);
-                start = te + 1;
-                end = te + 1;
+                start = ce + 1;
+                end = ce + 1;
             } else {
-                end = Math.max(te, end);
+                end = Math.max(ce, end);
             }
         }
-        //if last part not added to list
-        if (start != ch.length) {
-            res.add(end - start + 1);
-        }
+        //end for last element must be the length - 1 of string, so last char will be definitly updated
         return res;
     }
 }
